5b47281e179c5b0bdf8ed7128ce072a3f6077fe1
Dora <dora.nacinovic99@gmail.com>
2024-07-04 13:21:39 +0200

    changed card imgs and hero layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 const initialImgItems = [
-  "https://png.pngtree.com/thumb_back/fh260/background/20210814/pngtree-business-gradient-geometric-gray-square-abstract-simple-gray-background-image_759189.jpg",
-  "https://png.pngtree.com/thumb_back/fh260/background/20210811/pngtree-simple-rectangular-gray-background-image_759093.jpg",
-  "https://files.123freevectors.com/wp-content/original/131247-abstract-dark-grey-polygonal-background-design.jpg",
-  "https://images.unsplash.com/photo-1454117096348-e4abbeba002c?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGdyYXklMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww",
-  "https://png.pngtree.com/background/20211215/original/pngtree-abstract-gradient-white-gray-three-dimensional-triangle-technology-business-background-picture-image_1453067.jpg",
-  "https://motionarray.imgix.net/preview-33509CFka6iC1IY_0000.jpg?w=660&q=60&fit=max&auto=format",
-  "https://png.pngtree.com/thumb_back/fh260/background/20201015/pngtree-white-polygonal-mosaic-triangular-background-abstract-light-gray-background-low-poly-image_418752.jpg",
-  "https://static.vecteezy.com/system/resources/previews/006/045/135/original/abstract-modern-white-and-grey-chaotic-polygonal-background-free-vector.jpg"
+  "https://img.freepik.com/premium-photo/red-wallpapers-that-will-make-you-want-red-wallpapers_889227-20197.jpg",
+  "https://img.freepik.com/premium-vector/red-background-with-diamond-effect_23-2147618257.jpg",
+  "https://w0.peakpx.com/wallpaper/214/775/HD-wallpaper-white-and-red-waves-red-aesthetic.jpg",
+  "https://w0.peakpx.com/wallpaper/865/912/HD-wallpaper-red-wave-dark-blue-pink-flowers-plain-abstract-rose-maroon.jpg",
+  "https://png.pngtree.com/thumb_back/fw800/background/20231221/pngtree-white-background-with-abstract-red-lines-image_15547147.png",
+  "https://st4.depositphotos.com/24452164/26148/v/450/depositphotos_261486044-stock-illustration-red-and-white-diagonal-shiny.jpg",
+  "https://png.pngtree.com/thumb_back/fw800/background/20231002/pngtree-vibrant-red-abstract-background-with-a-captivating-3d-effect-image_13538232.png",
+  "https://t4.ftcdn.net/jpg/06/74/54/41/360_F_674544137_cYezvLnWGAU1HWrrSaGQBxE5pTzmjmEQ.jpg"
 ];
 
 const initialTitleItems = [
@@ -45,21 +45,22 @@ const HomePage = () => {
   };
 
   return (
-    <div className="container mx-auto p-8 mt-0 ">
+    <div className="container mx-auto p-12 mt-0 ">
       <div className="bg-red-700 shadow-md rounded-lg overflow-hidden flex flex-col p-5 mb-20 text-white">
         <p>SHOP NOW WITH LOREM IPSUM AND SAVE 10% AT CHECKOUT WITH CODE: <a  className="text-yellow-300"href="">*YL2T5S79D*</a>  </p>
         <p className="text-sm text-gray-400">Offer valid until 0x/0x/0x0x.</p> 
       </div>
             <h2 className="text-3xl font-bold text-center mb-14 text-white">SHOP LOREM IPSUM</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-14">
         {imgItems.map((img, index) => (
           <div key={index} className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col">
             <img src={img} alt={titleItems[index]} className="w-full h-48 object-cover"/>
             <div className="p-4 flex flex-col flex-grow justify-between">
               <div>
-                <h3 className="text-xl text-black font-semibold mb-2">{titleItems[index]}</h3>
+                <h3 className="text-xl text-black font-semibold mb-10">{titleItems[index]}      <hr /></h3>
+
               </div>
-              <div className="flex justify-between items-end mt-auto mt-5">
+              <div className="flex justify-between items-end mt-auto mt-7">
                 <p className="text-green-600 text-lg">{priceItems[index]}</p>
                 <button className="bg-red-600 rounded-lg p-2 text-white">Buy Now</button>
               </div>
